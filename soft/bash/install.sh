273ff7f4d4eca5993a34957b5cfec4d739b25526
Andrew Ivanov <and@hfrog.ru>
2017-07-19 11:39:10 +0000

    added soft/bash/install.sh

---
+++ soft/bash/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# vim: set sw=2 :
+
+if [[ -z ${1+x} ]]; then
+  echo "Usage: $0 <dst_dir>"
+  exit 1
+fi
+dst_dir=$1
+
+for f in dot*; do
+  dst=$dst_dir/${f#dot}
+  rm -f $dst
+  cp -v $f $dst
+done

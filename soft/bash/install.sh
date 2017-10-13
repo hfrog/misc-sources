ee368e189f61a1f313ec5e41a235156369e48103
Andrew Ivanov <and@hfrog.ru>
2017-10-13 17:19:23 +0300

    fixed bugs in soft/bash/install.sh

--- soft/bash/install.sh
+++ soft/bash/install.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 # vim: set sw=2 :
 
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+
+error_report() {
+  echo "errexit on line $(caller)" >&2
+}
+
+trap error_report ERR
+
 if [[ -z ${1+x} ]]; then
   echo "Usage: $0 <dst_dir>"
   exit 1
 fi
 dst_dir=$1
 
-for f in dot*; do
-  dst=$dst_dir/${f#dot}
+for f in $(dirname $BASH_SOURCE)/dot*; do
+  [ -f $f ]
+  d=$(basename $f)
+  dst=$dst_dir/${d#dot}
   rm -f $dst
   cp -v $f $dst
 done

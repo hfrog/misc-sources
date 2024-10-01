35a8b498cd49fe2f5524e9466f6bd634ddf0a73e
Andrew Ivanov <and@hfrog.ru>
2024-10-01 11:53:04 +0300

    Update client-server test apps

--- soft/client-server/tclient.cc
+++ soft/client-server/tclient.cc
@@ -1,5 +1,5 @@
 /*
- vim: ai:et:sw=2:nows
+ vim: ai:et:ic:sw=2:nows
 */
 
 #include <stdio.h>
@@ -15,6 +15,11 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <time.h>
+#include <limits.h>
+#include <netinet/tcp.h>
+
+// Don't allow timestaps to be sent to these ports. So we don't disturb prod web servers.
+#define WEB_PORTS { 80, 443 }
 
 int get_local_port(int sockfd) {
   struct sockaddr_in sa_in;
@@ -22,7 +27,7 @@ int get_local_port(int sockfd) {
 
   int sn = getsockname(sockfd, (struct sockaddr *)&sa_in, &sa_in_len);
   if (sn != 0) {
-    fprintf(stderr, "getsockname error: %d %s\n", errno, strerror(errno));
+    fprintf(stderr, "Getsockname error: %d %s\n", errno, strerror(errno));
     exit(EXIT_FAILURE);
   }
   return ntohs(sa_in.sin_port);
@@ -31,26 +36,126 @@ int get_local_port(int sockfd) {
 void get_clock(struct timespec *ts) {
   int res = clock_gettime(CLOCK_REALTIME, ts);
   if (res != 0) {
-    fprintf(stderr, "clock_gettime error: %s\n", strerror(errno));
+    fprintf(stderr, "Clock_gettime error: %s\n", strerror(errno));
     exit(EXIT_FAILURE);
   }
 //  printf("%ld.%09ld\n", ts->tv_sec, ts->tv_nsec);
 }
 
-uint64_t clock_diff(struct timespec *begin, struct timespec *end) {
+int64_t clock_diff(struct timespec *begin, struct timespec *end) {
   return (end->tv_sec - begin->tv_sec) * 1000000000 + end->tv_nsec - begin->tv_nsec;
 }
 
-int main(int argc, char **argv) {
+bool is_web_port(int port) {
+  int ports[] = WEB_PORTS;
+  for (int i=0; i<sizeof(ports)/sizeof(int); i++) {
+    if (port == ports[i]) {
+      return true;
+    }
+  }
+  return false;
+}
 
-  if (argc < 3) {
-    fprintf(stderr, "Usage: %s host port\n", argv[0]);
-    exit(-1);
+struct cmd_opts {
+  char host[64]        = { 0 }; // server to connect to
+  char port[8]         = { 0 }; // server port
+  bool send_timestamps = false; // send timestamps to the server upon connection
+  long repetitions     = 10;
+  long min_delay       = 0;
+  bool debug           = false;
+};
+
+void usage() {
+  fprintf(stderr, "Usage: %s [-d] [-h] [-m min_delay] [-n repetitions] [-t] host port\n", program_invocation_short_name);
+  fprintf(stderr, "  -d              Print some debug info\n");
+  fprintf(stderr, "  -h              Print this help\n");
+  fprintf(stderr, "  -m min_delay    Show connection times only when delay larger than or equal to min_delay, in microseconds. Default is 0\n");
+  fprintf(stderr, "  -n repetitions  Repeat connections. Default is 10\n");
+  fprintf(stderr, "  -t              Send timestamps\n");
+  exit(EXIT_FAILURE);
+}
+
+long parse_number(const char*str) {
+  if (strspn(str, "0123456789") != strlen(str)) {
+    fprintf(stderr, "Can't parse number %s\n", str);
+    usage();
   }
-  int repeat = 10;
-  if (argc > 3) {
-    repeat = atoi(argv[3]);
+  errno = 0;
+  long result = strtol(str, NULL, 10);
+  if (errno != 0) {
+    fprintf(stderr, "Strtol error: %d %s\n", errno, strerror(errno));
+    usage();
   }
+  return result;
+}
+
+void parse_command_line(struct cmd_opts *opts, int argc, char **argv) {
+  int opt;
+
+  while ((opt = getopt(argc, argv, "dhm:n:t")) != -1) {
+    switch (opt) {
+    case 'd':
+      opts->debug = true;
+      break;
+
+    case 'h':
+      usage();
+      break;
+
+    case 'm':
+      opts->min_delay = parse_number(optarg);
+      break;
+
+    case 'n':
+      opts->repetitions = parse_number(optarg);
+      break;
+
+    case 't':
+      opts->send_timestamps = true;
+      break;
+
+    default:
+      usage();
+    }
+  }
+
+  int nonopt_args = 0;
+  long parsed_port;
+  while (optind < argc) {
+    switch (nonopt_args) {
+    case 0: // host
+      strncpy(opts->host, argv[optind], sizeof(opts->host)-1);
+      break;
+
+    case 1: // port
+      parsed_port = parse_number(argv[optind]); // just to check the value
+      if (parsed_port > USHRT_MAX) {
+        fprintf(stderr, "Port must be less than %d, %ld given\n", USHRT_MAX, parsed_port);
+        usage();
+      }
+      // sanity check
+      if (opts->send_timestamps and is_web_port(parsed_port)) {
+        fprintf(stderr, "Won't send timestamps on web port %ld\n", parsed_port);
+        usage();
+      }
+      strncpy(opts->port, argv[optind], sizeof(opts->port)-1);
+      break;
+
+    default:
+      fprintf(stderr, "Unexpected arg %s\n", argv[optind]);
+      usage();
+    }
+    optind++;
+    nonopt_args++;
+  }
+  if (nonopt_args != 2) {
+    usage();
+  }
+}
+
+int main(int argc, char **argv) {
+  struct cmd_opts cmd_opts;
+  parse_command_line(&cmd_opts, argc, argv);
 
   struct addrinfo hints;
   memset(&hints, 0, sizeof(struct addrinfo));
@@ -58,27 +163,28 @@ int main(int argc, char **argv) {
   hints.ai_socktype = SOCK_STREAM;
 
   struct addrinfo *result;
-  int s = getaddrinfo(argv[1], argv[2], &hints, &result);
+  int s = getaddrinfo(cmd_opts.host, cmd_opts.port, &hints, &result);
   if (s != 0) {
-    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(s));
+    fprintf(stderr, "Getaddrinfo error: %s\n", gai_strerror(s));
     exit(EXIT_FAILURE);
   }
 
   for (struct addrinfo *rp = result; rp != NULL; rp = rp->ai_next) {
     struct timespec begin_ts, end_ts;
-    uint64_t t_diff;
+    int64_t t_diff;
 
     struct sockaddr_in *sa = (struct sockaddr_in *)(rp->ai_addr);
-//    fprintf(stderr, "Trying first addr %s:%d\n", inet_ntoa(sa->sin_addr), ntohs(sa->sin_port));
+    fprintf(stderr, "Connecting to %s:%d, %ld repetitions, %sshow only connections slower than %ldus\n", inet_ntoa(sa->sin_addr),
+        ntohs(sa->sin_port), cmd_opts.repetitions, cmd_opts.send_timestamps ? "sending timestamps, " : "", cmd_opts.min_delay);
 
     int i=0;
-    while (i++ < repeat) {
+    while (i++ < cmd_opts.repetitions) {
       int error = 0;
       std::string error_descr;
 
       int sd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
       if (sd == -1) {
-        fprintf(stderr, "socker error: %d %s\n", errno, strerror(errno));
+        fprintf(stderr, "Socker error: %d %s\n", errno, strerror(errno));
         exit(EXIT_FAILURE);
       }
 
@@ -89,25 +195,46 @@ int main(int argc, char **argv) {
           error = 1;
           error_descr.assign(strerror(errno));
         } else {
-          fprintf(stderr, "connect error: %d %s\n", errno, strerror(errno));
+          fprintf(stderr, "Connect error: %d %s\n", errno, strerror(errno));
           exit(EXIT_FAILURE);
         }
       }
       get_clock(&end_ts);
 
-      int sent = send(sd, &begin_ts, sizeof(struct timespec), 0);
-      if (sent == -1) {
-        fprintf(stderr, "send error: %d %s\n", errno, strerror(errno));
-        exit(EXIT_FAILURE);
+      bool ts_was_sent = false;
+      if (cmd_opts.send_timestamps) {
+        struct tcp_info tcp_info;
+        int tcp_info_len = sizeof(tcp_info);
+
+        if (getsockopt(sd, IPPROTO_TCP, TCP_INFO, &tcp_info, (socklen_t *)&tcp_info_len) == -1) {
+          fprintf(stderr, "Getsockopt error: %d %s\n", errno, strerror(errno));
+          exit(EXIT_FAILURE);
+        }
+
+        if (tcp_info.tcpi_state == TCP_ESTABLISHED) {
+          int sent_bytes = send(sd, &begin_ts, sizeof(struct timespec), 0);
+          if (sent_bytes == -1) {
+            fprintf(stderr, "Send error: %d %s\n", errno, strerror(errno));
+            exit(EXIT_FAILURE);
+          }
+          ts_was_sent = true;
+        }
       }
 
       t_diff = clock_diff(&begin_ts, &end_ts)/1000;
-//      if (t_diff >= 1000) {
-      if (t_diff >= 0) {
-        printf("%lu local port: %d%s\n", t_diff, get_local_port(sd), error ? (" (" + error_descr + ")").c_str() : "");
-      } else {
-        printf(".");
-        fflush(stdout);
+      if (t_diff >= cmd_opts.min_delay) {
+        printf("%ld ", t_diff);
+        if (ts_was_sent and cmd_opts.debug) {
+          printf("sent_ts: %ld.%09ld ", begin_ts.tv_sec, begin_ts.tv_nsec);
+        }
+        printf("local_port: %d", get_local_port(sd));
+        if (error) {
+          printf(" (%s)", error_descr.c_str());
+        }
+        printf("\n");
+//      } else {
+//        printf(".");
+//        fflush(stdout);
       }
       close(sd);
 
--- soft/client-server/tserver.cc
+++ soft/client-server/tserver.cc
@@ -1,5 +1,5 @@
 /*
- vim: ai:et:sw=2:nows
+ vim: ai:et:ic:sw=2:nows
 */
 
 #include <stdio.h>
@@ -14,8 +14,10 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <time.h>
+#include <limits.h>
 
 #define MAX_EVENTS 100000
+#define LISTEN_BACKLOG 65535
 
 int get_remote_port(int sockfd) {
   struct sockaddr_in sa_in;
@@ -23,7 +25,7 @@ int get_remote_port(int sockfd) {
 
   int sn = getpeername(sockfd, (struct sockaddr *)&sa_in, &sa_in_len);
   if (sn != 0) {
-    fprintf(stderr, "getpeername error: %d %s\n", errno, strerror(errno));
+    fprintf(stderr, "Getpeername error: %d %s\n", errno, strerror(errno));
     exit(EXIT_FAILURE);
   }
   return ntohs(sa_in.sin_port);
@@ -32,24 +34,30 @@ int get_remote_port(int sockfd) {
 void get_clock(struct timespec *ts) {
   int res = clock_gettime(CLOCK_REALTIME, ts);
   if (res != 0) {
-    fprintf(stderr, "clock_gettime error: %s\n", strerror(errno));
+    fprintf(stderr, "Clock_gettime error: %s\n", strerror(errno));
     exit(EXIT_FAILURE);
   }
 //  printf("%ld.%09ld\n", ts->tv_sec, ts->tv_nsec);
 }
 
-uint64_t clock_diff(struct timespec *begin, struct timespec *end) {
+int64_t clock_diff(struct timespec *begin, struct timespec *end) {
   return (end->tv_sec - begin->tv_sec) * 1000000000 + end->tv_nsec - begin->tv_nsec;
 }
 
 void setnonblocking(int sockfd) {
   if (fcntl(sockfd, F_SETFL, fcntl(sockfd, F_GETFD)|O_NONBLOCK) == -1) {
-    fprintf(stderr, "fcntl error: %d %s\n", errno, strerror(errno));
+    fprintf(stderr, "Fcntl error: %d %s\n", errno, strerror(errno));
     exit(EXIT_FAILURE);
   }
 }
 
-void read_fd(int fd) {
+struct cmd_opts {
+  char port[8]   = { 0 }; // listen port
+  long min_delay = 1000;
+  bool debug     = false;
+};
+
+void read_fd(int fd, const struct cmd_opts *cmd_opts) {
   char buf[1000];
   while (1) {
     int len = recv(fd, buf, sizeof(buf), 0);
@@ -57,33 +65,102 @@ void read_fd(int fd) {
       if (errno == EAGAIN) {
         break;
       } else {
-        fprintf(stderr, "recv error: %d %s\n", errno, strerror(errno));
+        fprintf(stderr, "Recv error: %d %s\n", errno, strerror(errno));
         exit(EXIT_FAILURE);
       }
     } else if (len == 0) {
-      // fprintf(stderr, "recv is zero, the socket is closed\n");
+      // fprintf(stderr, "Recv is zero, the socket is closed\n");
       close(fd);
       break;
     } else {
-      // normal read
-      struct timespec received_ts, current_ts;
+      struct timespec current_ts, received_ts;
       get_clock(&current_ts);
       memcpy((void *)&received_ts, buf, sizeof(struct timespec));
-//      printf("%ld.%09ld\n", received_ts.tv_sec, received_ts.tv_nsec);
-      uint64_t t_diff = clock_diff(&received_ts, &current_ts)/1000;
-      if (t_diff >= 1000) {
-        printf("%lu remote port: %d\n", t_diff, get_remote_port(fd));
+      int64_t t_diff = clock_diff(&received_ts, &current_ts)/1000;
+      if (t_diff >= cmd_opts->min_delay or t_diff < 0) {
+        printf("%ld ", t_diff);
+        if (cmd_opts->debug) {
+          printf("recv_ts: %ld.%09ld current_ts: %ld.%09ld ", received_ts.tv_sec, received_ts.tv_nsec, current_ts.tv_sec, current_ts.tv_nsec);
+        }
+        printf("remote_port: %d\n", get_remote_port(fd));
       }
     }
   }
 }
 
-int main(int argc, char **argv) {
+void usage() {
+  fprintf(stderr, "Usage: %s [-d] [-h] [-m min_delay] port\n", program_invocation_short_name);
+  fprintf(stderr, "  -d            Print some debug info\n");
+  fprintf(stderr, "  -h            Print this help\n");
+  fprintf(stderr, "  -m min_delay  Show connection times only when delay larger than or equal to min_delay, in microseconds. Default is 1000\n");
+  fprintf(stderr, "                Requires the client to send timestamps. Default is 1000\n");
+  exit(EXIT_FAILURE);
+}
 
-  if (argc < 2) {
-    fprintf(stderr, "Usage: %s port\n", argv[0]);
-    exit(-1);
+long parse_number(const char*str) {
+  if (strspn(str, "0123456789") != strlen(str)) {
+    fprintf(stderr, "Can't parse number %s\n", str);
+    usage();
   }
+  errno = 0;
+  long result = strtol(str, NULL, 10);
+  if (errno != 0) {
+    fprintf(stderr, "Strtol error: %d %s\n", errno, strerror(errno));
+    usage();
+  }
+  return result;
+}
+
+void parse_command_line(struct cmd_opts *opts, int argc, char **argv) {
+  int opt;
+
+  while ((opt = getopt(argc, argv, "dhm:")) != -1) {
+    switch (opt) {
+    case 'd':
+      opts->debug = true;
+      break;
+
+    case 'h':
+      usage();
+      break;
+
+    case 'm':
+      opts->min_delay = parse_number(optarg);
+      break;
+
+    default:
+      usage();
+    }
+  }
+
+  int nonopt_args = 0;
+  long parsed_port;
+  while (optind < argc) {
+    switch (nonopt_args) {
+    case 0: // port
+      parsed_port = parse_number(argv[optind]); // just to check the value
+      if (parsed_port > USHRT_MAX) {
+        fprintf(stderr, "Port must be less than %d, %ld given\n", USHRT_MAX, parsed_port);
+        usage();
+      }
+      strncpy(opts->port, argv[optind], sizeof(opts->port)-1);
+      break;
+
+    default:
+      fprintf(stderr, "Unexpected arg %s\n", argv[optind]);
+      usage();
+    }
+    optind++;
+    nonopt_args++;
+  }
+  if (nonopt_args != 1) {
+    usage();
+  }
+}
+
+int main(int argc, char **argv) {
+  struct cmd_opts cmd_opts;
+  parse_command_line(&cmd_opts, argc, argv);
 
   struct addrinfo hints;
   memset(&hints, 0, sizeof(struct addrinfo));
@@ -92,9 +169,9 @@ int main(int argc, char **argv) {
   hints.ai_flags    = AI_PASSIVE;
 
   struct addrinfo *ai;
-  int s = getaddrinfo(NULL, argv[1], &hints, &ai);
+  int s = getaddrinfo(NULL, cmd_opts.port, &hints, &ai);
   if (s != 0) {
-    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(s));
+    fprintf(stderr, "Getaddrinfo error: %s\n", gai_strerror(s));
     exit(EXIT_FAILURE);
   }
 
@@ -103,27 +180,27 @@ int main(int argc, char **argv) {
 
     int listen_sock = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
     if (listen_sock == -1) {
-      fprintf(stderr, "socker error: %d %s\n", errno, strerror(errno));
+      fprintf(stderr, "Socker error: %d %s\n", errno, strerror(errno));
       exit(EXIT_FAILURE);
     }
 
     setnonblocking(listen_sock);
 
     if (bind(listen_sock, rp->ai_addr, rp->ai_addrlen) == -1) {
-      fprintf(stderr, "bind error: %d %s\n", errno, strerror(errno));
+      fprintf(stderr, "Bind error: %d %s\n", errno, strerror(errno));
       exit(EXIT_FAILURE);
     }
 
-    if (listen(listen_sock, 65535) == -1) {
-      fprintf(stderr, "listen error: %d %s\n", errno, strerror(errno));
+    if (listen(listen_sock, LISTEN_BACKLOG) == -1) {
+      fprintf(stderr, "Listen error: %d %s\n", errno, strerror(errno));
       exit(EXIT_FAILURE);
     }
-    fprintf(stderr, "Listening on %s:%d\n", inet_ntoa(sa->sin_addr), ntohs(sa->sin_port));
+    fprintf(stderr, "Listening on %s:%d, show only connections slower than %ldus, if client sends timestamps\n", inet_ntoa(sa->sin_addr), ntohs(sa->sin_port), cmd_opts.min_delay);
 
     struct epoll_event ev, events[MAX_EVENTS];
     int epollfd = epoll_create1(0);
     if (epollfd == -1) {
-      fprintf(stderr, "epoll_create1 error: %d %s\n", errno, strerror(errno));
+      fprintf(stderr, "Epoll_create1 error: %d %s\n", errno, strerror(errno));
       exit(EXIT_FAILURE);
     }
 
@@ -131,14 +208,14 @@ int main(int argc, char **argv) {
     ev.data.fd = listen_sock;
 
     if (epoll_ctl(epollfd, EPOLL_CTL_ADD, listen_sock, &ev) == -1) {
-      fprintf(stderr, "epoll_ctl error: %d %s\n", errno, strerror(errno));
+      fprintf(stderr, "Epoll_ctl error: %d %s\n", errno, strerror(errno));
       exit(EXIT_FAILURE);
     }
 
     while (1) {
       int epoll_events_count = epoll_wait(epollfd, events, sizeof(events)/sizeof(struct epoll_event), -1);
       if (epoll_events_count == -1) {
-        fprintf(stderr, "epoll_wait error: %d %s\n", errno, strerror(errno));
+        fprintf(stderr, "Epoll_wait error: %d %s\n", errno, strerror(errno));
         exit(EXIT_FAILURE);
       }
 
@@ -149,7 +226,7 @@ int main(int argc, char **argv) {
 
           int conn_sock = accept(listen_sock, (struct sockaddr *)&remote_addr, &addrlen);
           if (conn_sock == -1) {
-            fprintf(stderr, "accept error: %d %s\n", errno, strerror(errno));
+            fprintf(stderr, "Accept error: %d %s\n", errno, strerror(errno));
             exit(EXIT_FAILURE);
           }
 //          fprintf(stderr, "Connection from %s:%d\n", inet_ntoa(remote_addr.sin_addr), ntohs(remote_addr.sin_port));
@@ -158,12 +235,12 @@ int main(int argc, char **argv) {
           ev.data.fd = conn_sock;
 
           if (epoll_ctl(epollfd, EPOLL_CTL_ADD, conn_sock, &ev) == -1) {
-            fprintf(stderr, "epoll_ctl error: %d %s\n", errno, strerror(errno));
+            fprintf(stderr, "Epoll_ctl error: %d %s\n", errno, strerror(errno));
             exit(EXIT_FAILURE);
           }
 
         } else {
-          read_fd(events[i].data.fd);
+          read_fd(events[i].data.fd, &cmd_opts);
         }
       }
     }
